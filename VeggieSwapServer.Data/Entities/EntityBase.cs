cffd5203cdbce5d6dbe8ccc04c01735fe078d11c
Kobe Delobelle <delobellekobe@gmail.com>
2021-05-16 19:09:07 +0200

    Data annotations

--- VeggieSwapServer.Data/Entities/EntityBase.cs
+++ VeggieSwapServer.Data/Entities/EntityBase.cs
@@ -5,7 +5,6 @@ namespace VeggieSwapServer.Data.Entities
 {
     public abstract class EntityBase
     {
-        [Required]
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
