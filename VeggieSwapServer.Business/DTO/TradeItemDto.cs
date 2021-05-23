c9b8b03d2f51eebe7febb203d32bd58b54f733cb
Dries Maes <dm.inbox@outlook.com>
2021-05-23 22:25:28 +0200

    added postalcode

--- VeggieSwapServer.Business/DTO/TradeItemDto.cs
+++ VeggieSwapServer.Business/DTO/TradeItemDto.cs
@@ -6,6 +6,7 @@
         public int UserId { get; set; }
         public string UserFirstName { get; set; }
         public string UserLastName { get; set; }
+        public string UserPostalCode { get; set; }
         public int ResourceId { get; set; }
         public string ResourceName { get; set; }
         public string ResourceImageUrl { get; set; }
